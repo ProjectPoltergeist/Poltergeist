dc69522d4209ee3dc735b5cefa70020f71763c45
KernelErr0r <villainkernelerror@gmail.com>
2021-02-10 14:26:22 +0100

    Create empty project

---
+++ Poltergeist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poltergeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
