c536ced43f29e60b1e1744ec66a037772aa77091
Freddson <67077079+Freddson@users.noreply.github.com>
2021-02-10 22:11:29 +0100

    Create CheatCheck.cs

---
+++ Poltergeist.Core/CheatCheck.cs
@@ -0,0 +1,9 @@
+int cheat = 0;
+        foreach (Process process in Process.GetProcessesByName("CheatEngine72"))
+        {
+            counter++;
+        }
+	if(cheat > 0)
+          {
+             //get code for closing Poltergeist process
+          }
