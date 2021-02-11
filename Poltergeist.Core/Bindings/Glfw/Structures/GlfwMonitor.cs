3d836a5a50b49e0df954ef4ef5b1b1cdf2bee87f
petris <paprikapolishgamer@gmail.com>
2021-02-11 10:49:44 +0100

    Add basic glfw bindings

---
+++ Poltergeist.Core/Bindings/Glfw/Structures/GlfwMonitor.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Poltergeist.Core.Bindings.Glfw.Structures
+{
+	[StructLayout(LayoutKind.Sequential)]
+	internal readonly struct GlfwMonitor
+	{
+	}
+}
