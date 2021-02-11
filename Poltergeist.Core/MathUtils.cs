6e1c7e98f1c0367da1ec93abc62b1f6c69ff848d
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2021-02-11 23:16:51 +0100

    Add NativeArray (#9)

---
+++ Poltergeist.Core/MathUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Poltergeist.Core
+{
+	public static class MathUtils
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Align(int value, int alignment) => (value + alignment - 1) / alignment * alignment;
+	}
+}
