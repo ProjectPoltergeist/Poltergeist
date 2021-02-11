c162f3f4f21e4c30397d9ca75eaf0e7a82ef229f
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2021-02-11 19:56:54 +0100

    Add exit handling (#6)

---
+++ Poltergeist.Core/ExitHandler.cs
@@ -0,0 +1,110 @@
+﻿using Mono.Unix;
+using Mono.Unix.Native;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Poltergeist.Core
+{
+	public static class ExitHandler
+	{
+		public static event Action Exit;
+
+		private static readonly object ExitLock = new();
+		private static readonly HandlerRoutine Routine = OnNativeSignal;
+
+		private static Process _currentProcess;
+		private static bool _called;
+
+		static ExitHandler()
+		{
+			_currentProcess = Process.GetCurrentProcess();
+			_currentProcess.EnableRaisingEvents = true;
+			_currentProcess.Exited += (_, _) => CallExit();
+			AppDomain.CurrentDomain.ProcessExit += (_, _) => CallExit();
+			AppDomain.CurrentDomain.DomainUnload += (_, _) => CallExit();
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				SetConsoleCtrlHandler(Routine, true);
+			}
+			else
+			{
+				new Thread(() =>
+				{
+					UnixSignal.WaitAny(new UnixSignal[]{
+						new(Signum.SIGINT),
+						new(Signum.SIGTERM),
+						new(Signum.SIGKILL),
+						new(Signum.SIGUSR1),
+						new(Signum.SIGUSR2),
+						new(Signum.SIGHUP)
+					}, -1);
+					CallExit();
+				})
+				{ IsBackground = true }.Start();
+			}
+		}
+
+		private static void CallExit()
+		{
+			lock (ExitLock)
+			{
+				if (_called)
+					return;
+
+				_called = true;
+				_currentProcess?.Dispose();
+				_currentProcess = null;
+				Exit?.Invoke();
+			}
+		}
+
+		public static void ExitProcess(int code = 0)
+		{
+			lock (ExitLock)
+			{
+				CallExit();
+				Environment.Exit(code);
+			}
+		}
+
+		private static bool OnNativeSignal(CtrlTypes ctrl)
+		{
+			CallExit();
+			return true;
+		}
+
+		[DllImport("Kernel32", EntryPoint = "SetConsoleCtrlHandler")]
+		private static extern bool SetConsoleCtrlHandler(HandlerRoutine handler, bool add);
+
+		[UnmanagedFunctionPointer(CallingConvention.Winapi)]
+		private delegate bool HandlerRoutine(CtrlTypes ctrlType);
+
+		private enum CtrlTypes
+		{
+			// ReSharper disable UnusedMember.Local
+			CtrlCEvent,
+			CtrlBreakEvent,
+			CtrlCloseEvent,
+			CtrlLogoffEvent,
+			CtrlShutdownEvent
+			// ReSharper restore UnusedMember.Local
+		}
+
+		public sealed class LifetimeHandle : IDisposable
+		{
+			public void Dispose()
+			{
+				CallExit();
+				GC.SuppressFinalize(this);
+			}
+
+			~LifetimeHandle()
+			{
+				CallExit();
+			}
+		}
+	}
+}
--- Poltergeist.Sandbox/Program.cs
+++ Poltergeist.Sandbox/Program.cs
@@ -1,40 +1,44 @@
-﻿using Poltergeist.Core.Bindings.Glfw;
+﻿using Poltergeist.Core;
+using Poltergeist.Core.Bindings.Glfw;
 using Poltergeist.Core.Bindings.Glfw.Structures;
 
 namespace Poltergeist.Sandbox
 {
-    public sealed class Program
-    {
-        public static unsafe void Main(string[] args)
-        {
-            const int glfwTrue = 1;
-            const int glfwFalse = 0;
-            const int glfwContextVersionMajor = 0x00022002;
-            const int glfwContextVersionMinor = 0x00022003;
-            const int glfwOpenGlProfile = 0x00022008;
-            const int glfwOpenGlCoreProfile = 0x00032001;
-            const int glfwOpenGlForwardCompat = 0x00022006;
-            const int glfwResizable = 0x00020003;
-            
-            GlfwNative.Init();
-            GlfwNative.WindowHint(glfwContextVersionMajor, 3);
-            GlfwNative.WindowHint(glfwContextVersionMinor, 3);
-            GlfwNative.WindowHint(glfwOpenGlProfile, glfwOpenGlCoreProfile);
-            GlfwNative.WindowHint(glfwOpenGlForwardCompat, glfwTrue);
-            GlfwNative.WindowHint(glfwResizable, glfwFalse);
-            
-            GlfwWindow* window = GlfwNative.CreateWindow(800, 600, "Poltergeist Editor", null, null);
+	public sealed class Program
+	{
+		public static unsafe void Main()
+		{
+			using (ExitHandler.LifetimeHandle _ = new())
+			{
+				const int glfwTrue = 1;
+				const int glfwFalse = 0;
+				const int glfwContextVersionMajor = 0x00022002;
+				const int glfwContextVersionMinor = 0x00022003;
+				const int glfwOpenGlProfile = 0x00022008;
+				const int glfwOpenGlCoreProfile = 0x00032001;
+				const int glfwOpenGlForwardCompat = 0x00022006;
+				const int glfwResizable = 0x00020003;
 
-            GlfwNative.MakeContextCurrent(window);
+				GlfwNative.Init();
+				GlfwNative.WindowHint(glfwContextVersionMajor, 3);
+				GlfwNative.WindowHint(glfwContextVersionMinor, 3);
+				GlfwNative.WindowHint(glfwOpenGlProfile, glfwOpenGlCoreProfile);
+				GlfwNative.WindowHint(glfwOpenGlForwardCompat, glfwTrue);
+				GlfwNative.WindowHint(glfwResizable, glfwFalse);
 
-            while (GlfwNative.WindowShouldClose(window) == glfwFalse)
-            {
-                GlfwNative.PollEvents();
+				GlfwWindow* window = GlfwNative.CreateWindow(800, 600, "Poltergeist Editor", null, null);
 
-                GlfwNative.SwapBuffers(window);
-            }
-            
-            GlfwNative.Terminate();
-        }
-    }
+				GlfwNative.MakeContextCurrent(window);
+
+				while (GlfwNative.WindowShouldClose(window) == glfwFalse)
+				{
+					GlfwNative.PollEvents();
+
+					GlfwNative.SwapBuffers(window);
+				}
+
+				GlfwNative.Terminate();
+			}
+		}
+	}
 }
