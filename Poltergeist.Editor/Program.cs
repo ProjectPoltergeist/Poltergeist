206382b671dfc61d3337abd2f0b60fc75bcd7acd
petris <paprikapolishgamer@gmail.com>
2021-02-11 22:47:12 +0100

    Add .editorconfig

--- Poltergeist.Editor/Program.cs
+++ Poltergeist.Editor/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Poltergeist.Editor
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
