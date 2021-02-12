4b9e6e983eadfcc8d303bd2be5bc62eb2ec4d94e
Dorian "KernelError <villainkernelerror@gmail.com>
2021-02-12 16:31:06 +0100

    Group OpenGL bindings by version (#12)

---
+++ Poltergeist.Core/Bindings/OpenGl/OpenGl2Native.cs
@@ -0,0 +1,14 @@
+﻿namespace Poltergeist.Core.Bindings.OpenGl
+{
+	public unsafe class OpenGl2Native : OpenGl1Native
+	{
+		public readonly delegate* unmanaged[Cdecl]<uint, int, int, int, int, void*, void> VertexAttributePointer;
+		public readonly delegate* unmanaged[Cdecl]<uint, void> EnableVertexAttributeArray;
+		
+		public OpenGl2Native(delegate*<string, void*> loader) : base(loader)
+		{
+			VertexAttributePointer = (delegate* unmanaged[Cdecl]<uint, int, int, int, int, void*, void>)loader("glVertexAttribPointer");
+			EnableVertexAttributeArray = (delegate* unmanaged[Cdecl]<uint, void>)loader("glEnableVertexAttribArray");
+		}
+	}
+}
