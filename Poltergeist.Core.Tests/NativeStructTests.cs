69d996add80548d6001e48bd0b4ffbb500befa26
petris <paprikapolishgamer@gmail.com>
2021-02-12 23:55:56 +0100

    Add NativeStruct

---
+++ Poltergeist.Core.Tests/NativeStructTests.cs
@@ -0,0 +1,72 @@
+﻿using Poltergeist.Core.Memory;
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Poltergeist.Core.Tests
+{
+	public class NativeStructTests
+	{
+		[Theory]
+		[InlineData(0, null, null, 0)]
+		[InlineData(int.MaxValue, "abcde", "fghij", int.MaxValue)]
+		[InlineData(-1, "abc\tde", "fgh\tij", -1)]
+		public unsafe void DataTest(int i, string a, string b, int p)
+		{
+			TestStruct ts = new() { a = a, b = b, i = i, p = new IntPtr(p) };
+			using (NativeStruct<TestStruct> nativeStruct = new(ts))
+			{
+				Assert.True(nativeStruct.Data != null);
+				Assert.NotNull(nativeStruct.Allocator);
+				Assert.Equal(Marshal.SizeOf<TestStruct>(), nativeStruct.Size);
+				Assert.Equal(ts.i, *(int*)nativeStruct.Data);
+				Assert.Equal(ts, Marshal.PtrToStructure<TestStruct>(new IntPtr(nativeStruct.Data)));
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct TestStruct : IEquatable<TestStruct>
+		{
+			public int i;
+			[MarshalAs(UnmanagedType.LPUTF8Str)]
+			public string a;
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public string b;
+			public IntPtr p;
+
+			public bool Equals(TestStruct other)
+			{
+				return i == other.i && a == other.a && b == other.b && p.Equals(other.p);
+			}
+
+			public override bool Equals(object obj)
+			{
+				return obj is TestStruct other && Equals(other);
+			}
+
+			public override int GetHashCode()
+			{
+				// ReSharper disable NonReadonlyMemberInGetHashCode
+				return HashCode.Combine(i, a, b, p);
+				// ReSharper restore NonReadonlyMemberInGetHashCode
+			}
+
+			public static bool operator ==(TestStruct left, TestStruct right)
+			{
+				return left.Equals(right);
+			}
+
+			public static bool operator !=(TestStruct left, TestStruct right)
+			{
+				return !left.Equals(right);
+			}
+
+			public override string ToString()
+			{
+				// ReSharper disable HeapView.BoxingAllocation
+				return $"({i}) ({a}) ({b}) ({p})";
+				// ReSharper restore HeapView.BoxingAllocation
+			}
+		}
+	}
+}
---
+++ Poltergeist.Core/Memory/NativeStruct.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Poltergeist.Core.Memory
+{
+	public sealed unsafe class NativeStruct<T> : IDisposable where T : struct
+	{
+		// ReSharper disable once StaticMemberInGenericType
+		public static readonly INativeAllocator DefaultAllocator = new CoTaskMemAllocator();
+
+		public readonly void* Data;
+		public readonly int Size;
+
+		internal readonly INativeAllocator Allocator;
+
+		private readonly bool _zeroOnFree;
+		private readonly object _disposeLock = new();
+
+		private bool _valid;
+
+		public NativeStruct(T structure, bool zeroOnFree = false, INativeAllocator allocator = null)
+		{
+			Size = Marshal.SizeOf<T>();
+
+			Allocator = allocator ?? DefaultAllocator;
+			IntPtr ptr = Allocator.Allocate(Size);
+			GC.AddMemoryPressure(Size);
+
+			Marshal.StructureToPtr(structure, ptr, false);
+			Data = ptr.ToPointer();
+
+			_valid = Data != null;
+			_zeroOnFree = zeroOnFree;
+		}
+
+		private void Free()
+		{
+			lock (_disposeLock)
+			{
+				if (!_valid)
+					return;
+				IntPtr ptr = new(Data);
+
+				Marshal.DestroyStructure<T>(ptr);
+				if (_zeroOnFree)
+					new Span<byte>(Data, Size).Clear();
+				Allocator.Free(ptr);
+				GC.RemoveMemoryPressure(Size);
+
+				_valid = false;
+			}
+		}
+
+		public void Dispose()
+		{
+			Free();
+			GC.SuppressFinalize(this);
+		}
+
+		~NativeStruct()
+		{
+			Free();
+		}
+	}
+}
