b2c7c71ee34c2564ecc2931505475b7454c46f73
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2021-02-14 18:37:12 +0100

    OpenGL IsSupported + small cleanup (#19)

--- Poltergeist.Core/Bindings/OpenGl/OpenGl2Native.cs
+++ Poltergeist.Core/Bindings/OpenGl/OpenGl2Native.cs
@@ -4,11 +4,34 @@ namespace Poltergeist.Core.Bindings.OpenGl
 {
 	public abstract unsafe class OpenGl2Native : OpenGl1Native
 	{
+		public new static readonly bool IsSupported;
+
+		public static readonly delegate* unmanaged[Cdecl]<int, int*, void> GetIntegerV;
+
 		public static readonly delegate* unmanaged[Cdecl]<uint, int, int, int, int, nuint, void> VertexAttributePointer
 			= (delegate* unmanaged[Cdecl]<uint, int, int, int, int, nuint, void>)GlfwNative.GetProcessAddress(
 				"glVertexAttribPointer");
 
 		public static readonly delegate* unmanaged[Cdecl]<uint, void> EnableVertexAttributeArray
 			= (delegate* unmanaged[Cdecl]<uint, void>)GlfwNative.GetProcessAddress("glEnableVertexAttribArray");
+
+		static OpenGl2Native()
+		{
+			GetIntegerV = (delegate* unmanaged[Cdecl]<int, int*, void>)GlfwNative.GetProcessAddress("glGetIntegerv");
+			IsSupported = OpenGl1Native.IsSupported && GetIntegerV != null;
+		}
+
+		public static (int major, int minor) GetVersion()
+		{
+			if (GetIntegerV == null)
+				return (0, 0);
+			int major = 0;
+			const int glMajorVersion = 0x821B;
+			GetIntegerV(glMajorVersion, &major);
+			int minor = 0;
+			const int glMinorVersion = 0x821C;
+			GetIntegerV(glMinorVersion, &minor);
+			return (major, minor);
+		}
 	}
 }
--- Poltergeist.Core/Bindings/OpenGl/OpenGl3Native.cs
+++ Poltergeist.Core/Bindings/OpenGl/OpenGl3Native.cs
@@ -4,13 +4,15 @@ namespace Poltergeist.Core.Bindings.OpenGl
 {
 	public abstract unsafe class OpenGl3Native : OpenGl2Native
 	{
-		public static readonly delegate* unmanaged[Cdecl]<int, uint*, void> GenerateVertexArrays 
+		public new static readonly bool IsSupported = OpenGl2Native.IsSupported && GetVersion().major >= 3;
+
+		public static readonly delegate* unmanaged[Cdecl]<int, uint*, void> GenerateVertexArrays
 			= (delegate* unmanaged[Cdecl]<int, uint*, void>)GlfwNative.GetProcessAddress("glGenVertexArrays");
-		
-		public static readonly delegate* unmanaged[Cdecl]<int, uint*, void> DeleteVertexArrays 
+
+		public static readonly delegate* unmanaged[Cdecl]<int, uint*, void> DeleteVertexArrays
 			= (delegate* unmanaged[Cdecl]<int, uint*, void>)GlfwNative.GetProcessAddress("glDeleteVertexArrays");
-		
-		public static readonly delegate* unmanaged[Cdecl]<uint, void> BindVertexArray 
+
+		public static readonly delegate* unmanaged[Cdecl]<uint, void> BindVertexArray
 			= (delegate* unmanaged[Cdecl]<uint, void>)GlfwNative.GetProcessAddress("glBindVertexArray");
 	}
 }
