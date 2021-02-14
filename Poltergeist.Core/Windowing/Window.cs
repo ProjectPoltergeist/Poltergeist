5417f23abeb704e1b7872883baf97fa7cf79ac02
KernelErr0r <villainkernelerror@gmail.com>
2021-02-14 15:31:26 +0100

    Implement full dispose pattern for Window

--- Poltergeist.Core/Windowing/Window.cs
+++ Poltergeist.Core/Windowing/Window.cs
@@ -7,20 +7,34 @@ namespace Poltergeist.Core.Windowing
 	public sealed unsafe class Window : IDisposable
 	{
 		private readonly GlfwWindow* _window;
-		
+
 		public bool IsOpen => GlfwNative.WindowShouldClose(_window) == 0;
-		
+
 		public Window(string title, int width = 800, int height = 600)
 		{
 			_window = GlfwNative.CreateWindow(width, height, title, null, null);
-			
+
 			GlfwNative.MakeContextCurrent(_window);
 		}
 
 		public void PollEvents() => GlfwNative.PollEvents();
-		
+
 		public void SwapBuffers() => GlfwNative.SwapBuffers(_window);
-		
-		public void Dispose() => GlfwNative.DestroyWindow(_window);
+
+		public void Dispose()
+		{
+			ReleaseUnmanagedResources();
+			GC.SuppressFinalize(this);
+		}
+
+		private void ReleaseUnmanagedResources()
+		{
+			GlfwNative.DestroyWindow(_window);
+		}
+
+		~Window()
+		{
+			ReleaseUnmanagedResources();
+		}
 	}
 }
