8fb54e639a4211118604cced114e943b0a92e21c
Dorian "KernelError <villainkernelerror@gmail.com>
2021-02-14 16:10:11 +0100

    Add simple abstraction for windows (#18)
    Co-authored-by: MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>

---
+++ Poltergeist.Core/Windowing/Window.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Poltergeist.Core.Bindings.Glfw;
+using Poltergeist.Core.Bindings.Glfw.Structures;
+
+namespace Poltergeist.Core.Windowing
+{
+	public sealed unsafe class Window : IDisposable
+	{
+		private readonly GlfwWindow* _window;
+		private readonly object _lock = new();
+		
+		private bool _disposed;
+
+		public bool IsOpen => GlfwNative.WindowShouldClose(_window) == 0;
+
+		public Window(string title, int width = 800, int height = 600)
+		{
+			_window = GlfwNative.CreateWindow(width, height, title, null, null);
+
+			GlfwNative.MakeContextCurrent(_window);
+		}
+
+		public void PollEvents() => GlfwNative.PollEvents();
+
+		public void SwapBuffers() => GlfwNative.SwapBuffers(_window);
+
+		public void Dispose()
+		{
+			Release();
+			GC.SuppressFinalize(this);
+		}
+
+		private void Release()
+		{
+			lock (_lock)
+			{
+				if (_disposed)
+					return;
+				GlfwNative.DestroyWindow(_window);
+				_disposed = true;
+			}
+		}
+
+		~Window()
+		{
+			Release();
+		}
+	}
+}
