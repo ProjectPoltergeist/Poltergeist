311b006e0a8499b691f9f41975c01a0386a52c1b
Crawcik <crawcik@gmail.com>
2021-02-14 21:37:17 +0100

    Input enums

---
+++ Poltergeist.Core/Bindings/Glfw/Enums/GlfwMouse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poltergeist.Core.Bindings.Glfw.Enums
+{
+	public enum Mouse : int
+	{
+		MOUSE_BUTTON_1 = 0,
+
+		MOUSE_BUTTON_2 = 1,
+
+		MOUSE_BUTTON_3 = 2,
+
+		MOUSE_BUTTON_4 = 3,
+
+		MOUSE_BUTTON_5 = 4,
+
+		MOUSE_BUTTON_6 = 5,
+
+		MOUSE_BUTTON_7 = 6,
+
+		MOUSE_BUTTON_8 = 7,
+	}
+}
