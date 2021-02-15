3f2cc26f69c20bf4bbe25c3b5588ac2450f52cd0
Crawcik <crawcik@gmail.com>
2021-02-15 11:39:20 +0100

    I forgot again

--- Poltergeist.Core/Bindings/Glfw/GlfwError.cs
+++ Poltergeist.Core/Bindings/Glfw/GlfwError.cs
@@ -2,26 +2,26 @@
 {
 	public enum GlfwError
 	{
-		NO_ERROR = 0x00000000,
+		NoError = 0x00000000,
 
-		NOT_INITIALIZED = 0x00010001,
+		NotInitialized = 0x00010001,
 
-		NO_CURRENT_CONTEXT = 0x00010002,
+		NoCurrentContext = 0x00010002,
 
-		INVALID_ENUM = 0x00010003,
+		InvalidEnum = 0x00010003,
 
-		INVALID_VALUE = 0x00010004,
+		InvalidValue = 0x00010004,
 
-		OUT_OF_MEMORY = 0x00010005,
+		OutOfMemory = 0x00010005,
 
-		API_UNAVAILABLE = 0x00010006,
+		ApiUnavailable = 0x00010006,
 
-		VERSION_UNAVAILABLE = 0x00010007,
+		VersionUnavailable = 0x00010007,
 
-		PLATFORM_ERROR = 0x00010008,
+		PlatformError = 0x00010008,
 
-		FORMAT_UNAVAILABLE = 0x00010009,
+		FormatUnavailable = 0x00010009,
 
-		NO_WINDOW_CONTEXT = 0x0001000A,
+		NoWindowContext = 0x0001000A,
 	}
 }
