fc00a6b90f9efd7c1071b8b77ee23e45c999aa26
Crawcik <crawcik@gmail.com>
2021-02-15 23:31:52 +0100

    Added Callbacks

---
+++ Poltergeist.Core/Bindings/Glfw/Callbacks/GlfwWindowCloseFun.cs
@@ -0,0 +1,10 @@
+﻿using Poltergeist.Core.Bindings.Glfw.Structures;
+
+namespace Poltergeist.Core.Bindings.Glfw.Callbacks
+{
+	public unsafe struct GlfwWindowCloseFun
+	{
+		public delegate void WindowCloseCallback(GlfwWindow* window, int x, int y);
+		public readonly WindowCloseCallback windowCloseCallback;
+	}
+}
---
+++ Poltergeist.Core/Bindings/Glfw/Callbacks/GlfwWindowPosFun.cs
@@ -0,0 +1,10 @@
+﻿using Poltergeist.Core.Bindings.Glfw.Structures;
+
+namespace Poltergeist.Core.Bindings.Glfw.Callbacks
+{
+	public unsafe struct GlfwWindowPosFun
+	{
+		public  delegate void WindowPosCallback(GlfwWindow* window, int x, int y);
+		public readonly WindowPosCallback windowPosCallback;
+	}
+}
