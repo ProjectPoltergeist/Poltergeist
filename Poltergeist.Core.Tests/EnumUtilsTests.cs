badabd54e1bfb3d865a201ff89480a548f3a3758
petris <paprikapolishgamer@gmail.com>
2021-02-18 22:49:54 +0100

    Add tests

---
+++ Poltergeist.Core.Tests/EnumUtilsTests.cs
@@ -0,0 +1,65 @@
+﻿using Poltergeist.Core.Math;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Poltergeist.Core.Tests
+{
+	public static class EnumUtilsTests
+	{
+		// ReSharper disable UnusedMember.Local
+		private enum TestEnum
+		{
+			A = 1,
+			B = 2,
+			C = 4,
+			D = A,
+			E = B,
+			F = 3
+		}
+
+		[Flags]
+		public enum TestFlags
+		{
+			A = 1 << 0,
+			B = 1 << 1,
+			C = 1 << 2,
+			D = 1 << 3,
+			E = 1 << 4,
+			F = 1 << 5
+		}
+		// ReSharper restore UnusedMember.Local
+
+		[Fact]
+		public static void ValuesTest()
+		{
+			Assert.True(EnumUtils<TestEnum>.Values.ToArray().SequenceEqual(Enum.GetValues<TestEnum>()));
+		}
+
+		[Fact]
+		public static void NamesTest()
+		{
+			Assert.True(EnumUtils<TestEnum>.Names.ToArray().SequenceEqual(Enum.GetNames<TestEnum>()));
+		}
+
+		[Fact]
+		public static void TypeTest()
+		{
+			Assert.Equal(Enum.GetUnderlyingType(typeof(TestEnum)), EnumUtils<TestEnum>.UnderlyingType);
+		}
+
+		[Theory]
+		[InlineData((TestFlags)0, TestFlags.C)]
+		[InlineData(TestFlags.A, TestFlags.C)]
+		[InlineData(TestFlags.C, TestFlags.C)]
+		[InlineData(TestFlags.A | TestFlags.B, TestFlags.C)]
+		[InlineData(TestFlags.A | TestFlags.C, TestFlags.C)]
+		public static void HasFlagTest(TestFlags value, TestFlags flag)
+		{
+			// ReSharper disable HeapView.BoxingAllocation
+			Assert.Equal(value.HasFlag(flag), EnumUtils<TestFlags>.HasFlag(value, flag));
+			Assert.Equal(value.HasFlag(flag), value.HasFlagF(flag));
+			// ReSharper restore HeapView.BoxingAllocation
+		}
+	}
+}
--- Poltergeist.Core/Math/EnumUtils.cs
+++ Poltergeist.Core/Math/EnumUtils.cs
@@ -5,26 +5,13 @@ namespace Poltergeist.Core.Math
 {
 	public static class EnumUtils<T> where T : unmanaged, Enum
 	{
-		private static readonly T[] values = Enum.GetValues<T>();
-		private static readonly string[] names = Enum.GetNames<T>();
+		private static readonly T[] ValuesCache = Enum.GetValues<T>();
+		private static readonly string[] NamesCache = Enum.GetNames<T>();
 
-		public static ReadOnlySpan<T> Values => values;
-		public static ReadOnlySpan<string> Names => names;
+		public static ReadOnlySpan<T> Values => ValuesCache;
+		public static ReadOnlySpan<string> Names => NamesCache;
 		public static Type UnderlyingType { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; } = Enum.GetUnderlyingType(typeof(T));
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool IsDefined(T value) => Enum.IsDefined(value);
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static string GetName(T value) => Enum.GetName(value);
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T Parse(string value) => Enum.Parse<T>(value);
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T Parse(string value, bool ignoreCase) => Enum.Parse<T>(value, ignoreCase);
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool TryParse(string value, out T enu) => Enum.TryParse(value, out enu);
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool TryParse(string value, bool ignoreCase, out T enu) => Enum.TryParse(value, ignoreCase, out enu);
 #pragma warning disable CA2248
 		// ReSharper disable once HeapView.PossibleBoxingAllocation
 		// ReSharper disable once HeapView.BoxingAllocation
