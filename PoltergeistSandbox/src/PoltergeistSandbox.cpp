5f6428bf99514ed24d149c29f45574f3f14d6945
KernelErr0r <villainkernelerror@gmail.com>
2021-02-23 11:51:56 +0100

    Draw first triangle in sandbox

--- PoltergeistSandbox/src/PoltergeistSandbox.cpp
+++ PoltergeistSandbox/src/PoltergeistSandbox.cpp
@@ -31,10 +31,34 @@ int main() {
             return -2;
         }
 
+        uint32_t vertexArrayId;
+        glGenVertexArrays(1, &vertexArrayId);
+        glBindVertexArray(vertexArrayId);
+
+        float vertices[] = {
+                -0.5f, -0.5f, 0.0f,
+                 0.5f, -0.5f, 0.0f,
+                 0.0f,  0.5f, 0.0f
+        };
+
+        uint32_t vertexBufferId;
+        glGenBuffers(1, &vertexBufferId);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), &vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
         while (!glfwWindowShouldClose(window.get())) {
             glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
             glfwSwapBuffers(window.get());
         }
+
+        glDeleteBuffers(1, &vertexBufferId);
+        glDeleteVertexArrays(1, &vertexArrayId);
     }
 
     glfwTerminate();
