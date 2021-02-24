83743cc88a2dc73c824bf8b4cae4a434c68c2253
KernelErr0r <villainkernelerror@gmail.com>
2021-02-24 20:57:21 +0100

    Use index buffer, draw square

--- PoltergeistSandbox/src/PoltergeistSandbox.cpp
+++ PoltergeistSandbox/src/PoltergeistSandbox.cpp
@@ -36,27 +36,42 @@ int main() {
         glBindVertexArray(vertexArrayId);
 
         float vertices[] = {
-                -0.5f, -0.5f, 0.0f,
+                 0.5f,  0.5f, 0.0f,
                  0.5f, -0.5f, 0.0f,
-                 0.0f,  0.5f, 0.0f
+                -0.5f, -0.5f, 0.0f,
+                -0.5f,  0.5f, 0.0f
         };
 
         uint32_t vertexBufferId;
         glGenBuffers(1, &vertexBufferId);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), &vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        uint32_t indices[] = {
+                0, 1, 3,
+                1, 2, 3
+        };
+
+        uint32_t indexBufferId;
+        glGenBuffers(1, &indexBufferId);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
 
         while (!glfwWindowShouldClose(window.get())) {
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT);
             glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
             glfwSwapBuffers(window.get());
         }
 
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        glDeleteBuffers(1, &indexBufferId);
         glDeleteBuffers(1, &vertexBufferId);
         glDeleteVertexArrays(1, &vertexArrayId);
     }
