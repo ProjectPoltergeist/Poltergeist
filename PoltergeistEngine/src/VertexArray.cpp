16b0e160e60209cd9a881a905e22a066dc8b730f
ToamszJaworski <zir3xx@gmail.com>
2021-03-10 14:39:34 +0100

    Update VertexArray.cpp

--- PoltergeistEngine/src/VertexArray.cpp
+++ PoltergeistEngine/src/VertexArray.cpp
@@ -2,20 +2,20 @@
 
 VertexArray::VertexArray() noexcept
 {
-    glGenVertexArrays(1, &m_vertexArrayId);
+      glGenVertexArrays(1, &m_vertexArrayId);
 }
 
 VertexArray::~VertexArray() noexcept
 {
-    glDeleteVertexArrays(1, &m_vertexArrayId);
+      glGenVertexArrays(1, &m_vertexArrayId);
 }
 
 void VertexArray::Bind() const noexcept
 {
-    glBindVertexArray(m_vertexArrayId);
+      glBindVertexArray(m_vertexArrayId);
 }
 
 void VertexArray::Unbind() const noexcept
 {
-    glBindVertexArray(0);
+      glBindVertexArray(m_vertexArrayId);
 }
