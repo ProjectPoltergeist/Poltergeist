8ceb10e0dc5a741ea92f5fae30080af9ed854048
ToamszJaworski <zir3xx@gmail.com>
2021-03-21 03:46:31 +0100

    jpeg format support

---
+++ PoltergeistEngine/src/PoltergeistEngine/Image/JpegImage.cpp
@@ -0,0 +1,66 @@
+﻿#include "PoltergeistEngine/Image/JpegImage.hpp"
+#include <jpeglib.h>
+#include <iostream>
+#include <csetjmp>
+
+struct my_error_mgr
+{
+    jpeg_error_mgr publicErrorManager;
+    jmp_buf setJmpBuffer;
+};
+
+void my_error_exit(j_common_ptr cinfo)
+{
+    my_error_mgr* error = (my_error_mgr*)cinfo->err;
+    (*cinfo->err->output_message) (cinfo);
+    longjmp(error->setJmpBuffer, 1);
+}
+
+bool JpegImage::IsValidFormat(FILE* file)
+{
+	uint16_t header;
+	fread(&header, 1, 2, file);
+	fseek(file, -2, SEEK_CUR);
+	return header == 0xD8FF;
+}
+
+void JpegImage::LoadImageFromFile(FILE* file, uint32_t& width, uint32_t& height, uint8_t*& data)
+{
+    my_error_mgr error;
+    jpeg_decompress_struct decompressInfo;
+    JSAMPARRAY pixelBuffor;
+
+    decompressInfo.err = jpeg_std_error(&error.publicErrorManager);
+    error.publicErrorManager.error_exit = my_error_exit;
+    if (setjmp(error.setJmpBuffer))
+    {
+        jpeg_destroy_decompress(&decompressInfo);
+        throw std::runtime_error("jpg:jmp error");
+    }
+
+    jpeg_create_decompress(&decompressInfo);
+    jpeg_stdio_src(&decompressInfo, file);
+    jpeg_read_header(&decompressInfo, TRUE);
+    jpeg_start_decompress(&decompressInfo);
+
+    int rowLength = decompressInfo.output_width * decompressInfo.output_components;
+    pixelBuffor = (*decompressInfo.mem->alloc_sarray)
+        ((j_common_ptr)&decompressInfo, JPOOL_IMAGE, rowLength, 1);
+
+    data = new uint8_t[decompressInfo.output_width * decompressInfo.output_height * 3];
+    size_t data_offest = 0;
+    while (decompressInfo.output_scanline < decompressInfo.output_height)
+    {
+        jpeg_read_scanlines(&decompressInfo, pixelBuffor, 1);
+        memcpy(data + data_offest, pixelBuffor[0], rowLength);
+        data_offest += rowLength;
+    }
+
+    jpeg_finish_decompress(&decompressInfo);
+    jpeg_destroy_decompress(&decompressInfo);
+
+    if (error.publicErrorManager.num_warnings) throw std::runtime_error("Decompressing error");
+
+    width = decompressInfo.output_width;
+    height = decompressInfo.output_height;
+}
