23d2a8609d5eda5eb6309988f1e048a025831ce6
KernelErr0r <villainkernelerror@gmail.com>
2021-08-11 01:02:56 +0200

    Prevent evil from winning

--- PoltergeistEditor/include/ImGUIContent.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef POLTERGEIST_IMGUICONTENT_HPP
-#define POLTERGEIST_IMGUICONTENT_HPP
-
-void GUIContent();
-
-#endif
--- PoltergeistEngine/include/PoltergeistEngine/Encoding/EncodingUtilities.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef POLTERGEIST_ENCODINGUTILITIES_HPP
-#define POLTERGEIST_ENCODINGUTILITIES_HPP
-
-#ifdef WIN32
-wchar_t* ConvertUtf8ToUtf16(const char* data);
-char* ConvertUtf16ToUtf8(const wchar_t* data);
-#endif
-
-#endif
